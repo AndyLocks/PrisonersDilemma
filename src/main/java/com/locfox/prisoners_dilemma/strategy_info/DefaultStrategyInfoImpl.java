2f3d2f3acfc49d1a1140b5176716c4e7823361ae
AndyLocks <jandylokc@gmail.com>
2024-12-08 21:37:02 +0100

    Add (equals, hashCode)

--- src/main/java/com/locfox/prisoners_dilemma/strategy_info/DefaultStrategyInfoImpl.java
+++ src/main/java/com/locfox/prisoners_dilemma/strategy_info/DefaultStrategyInfoImpl.java
@@ -1,5 +1,7 @@
 package com.locfox.prisoners_dilemma.strategy_info;
 
+import java.util.Objects;
+
 public record DefaultStrategyInfoImpl(String name, String description) implements StrategyInfo {
 
     public static DefaultStrategyInfoImpl of(final String name, final String description) {
@@ -9,6 +11,18 @@ public record DefaultStrategyInfoImpl(String name, String description) implement
     @Override
     public String toString() {
         return String.format("Name [%s], description [%s]", name(), description());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        DefaultStrategyInfoImpl that = (DefaultStrategyInfoImpl) o;
+        return Objects.equals(name, that.name) && Objects.equals(description, that.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, description);
     }
 
 }
