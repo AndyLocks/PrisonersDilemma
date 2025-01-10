33277fb45003cfd96ec95379749a1cb620ead2c5
AndyLocks <jandylokc@gmail.com>
2025-01-10 21:23:18 +0100

    Add: equals and hashCode methods

--- src/main/java/com/locfox/prisoners_dilemma/strategy_info/StrategyInfo.java
+++ src/main/java/com/locfox/prisoners_dilemma/strategy_info/StrategyInfo.java
@@ -10,12 +10,15 @@ public interface StrategyInfo {
     /// @return description of the strategy
     String description();
 
+    boolean equals(Object obj);
+    int hashCode();
+
     /// Returns an implementation of an object containing strategy information.
     /// This implementation is standard for the entire application.
     ///
     /// @param name        a name of the strategy
     /// @param description a description of the strategy.
-    ///                    The description should contain a description of how the strategy works
+    ///                                       The description should contain a description of how the strategy works
     /// @return a default implementation
     static StrategyInfo withDefaults(String name, String description) {
         return DefaultStrategyInfoImpl.of(name, description);
